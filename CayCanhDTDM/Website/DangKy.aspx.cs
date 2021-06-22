3d54f8f94aae39e3fc48a85b19dfc4404906e40b
Võ Thanh Bình <1824801030056@student.tdmu.edu.vn>
2021-06-22 11:35:20 +0700

    a

---
+++ CayCanhDTDM/Website/DangKy.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Website
+{
+    public partial class DangKy : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnDN_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                string ktr = @"select 1 from taikhoan where tendn=N'" + tbTK.Text + "'";
+                if (XLDL.GetData(ktr).Rows.Count > 0)
+                {
+                    lbThongBao.Text = "Tên tài khoản đã tồn tại!!!";
+                    tbTK.Focus();
+                }
+                else
+                {
+                    string tentk = tbTenTK.Text;
+                    string email = tbEmail.Text;
+                    string dc = tbDiaChi.Text;
+                    string sdt = tbSDT.Text;
+                    string tendn = tbTK.Text;
+                    string mk = tbMK.Text;
+                    string sql = "insert into Taikhoan  values (N'" + tentk + "','" + email + "',N'" + dc + "','" + sdt + "','" + tendn + "','" + mk + "',2)";
+                    XLDL.Excute(sql);
+                    Response.Redirect("DangNhap.aspx");
+                }
+            }
+            catch
+            {
+                lbThongBao.Text = "Thất bại";
+            }
+            
+            
+            
+        }
+    }
+}
---
+++ CayCanhDTDM/Website/SuaDH.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Website
+{
+    public partial class SuaDH : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                loaddata();
+            }
+        }
+
+        private void loaddata()
+        {
+            if (Request.QueryString["MaDH"] != null)
+            {
+
+                string MaBlog = Request.QueryString["MaDH"].ToString();
+
+                string sql = "select MaDH,NgayLapDH,TenKH,DiaChi,SDT,PTTT,ThanhTien from DonHang Where MaDH=" + MaBlog;
+
+                DataTable dt = XLDL.GetData(sql);
+                lbNgayLapDH.Text = dt.Rows[0]["NgayLapDH"].ToString();
+                tbMaDH.Text = MaBlog;
+                tbTenKH.Text = dt.Rows[0]["TenKH"].ToString();
+                tbDiaChi.Text = dt.Rows[0]["DiaCHi"].ToString();
+                tbSDT.Text = dt.Rows[0]["SDT"].ToString();
+                tbPTTT.Text = dt.Rows[0]["PTTT"].ToString();
+                tbThanhTien.Text = dt.Rows[0]["ThanhTien"].ToString();
+
+                string sql1 = "SELECT SANPHAM.MaSP, SANPHAM.TenSP, SANPHAM.TienSP, ChiTietDonHang.SL, ChiTietDonHang.MaDH , SANPHAM.TienSP* ChiTietDonHang.SL AS ThanhTien FROM SANPHAM INNER JOIN ChiTietDonHang ON SANPHAM.MaSP = ChiTietDonHang.MaSP WHERE ChiTietDonHang.MaDH = " + MaBlog;
+
+                DataTable dt1 = XLDL.GetData(sql1);
+                gvGioHang.DataSource = dt1;
+                gvGioHang.DataBind();
+
+
+
+
+            }
+        }
+
+        protected void btnLuu_Click(object sender, EventArgs e)
+        {
+
+
+
+            string MaBlog = Request.QueryString["MaDH"].ToString();
+            string ngay = DateTime.Now.ToString("MM/dd/yyyy");
+
+            string ten = tbTenKH.Text;
+            string dc = tbDiaChi.Text;
+            string sdt = tbSDT.Text;
+            string pttt = tbPTTT.Text;
+            int tt = int.Parse(tbThanhTien.Text);
+
+
+
+            string sql = "update DonHang set NgayLapDH='" + ngay + "',TenKH=N'" + ten + "',DiaChi=N'" + dc + "',SDT='" + sdt + "',PTTT=N'" + pttt + "',ThanhTien="+tt+" where MaDH=" + MaBlog;
+            XLDL.Excute(sql);
+
+            Response.Redirect("DonHang.Aspx");
+        }
+
+        protected void gvGioHang_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gvGioHang.PageIndex = e.NewPageIndex;
+            gvGioHang.DataBind();
+        }
+    }
+    }
